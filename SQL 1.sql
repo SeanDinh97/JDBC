fc3fdf04f3e4e2f9215fc98d037ce5e8b44c5ef4
SeanDinh97 <37059446+SeanDinh97@users.noreply.github.com>
2018-03-18 14:13:05 -0700

    Update SQL 1.sql

--- SQL 1.sql
+++ SQL 1.sql
@@ -1,299 +1,80 @@
-package cecs.pkg323.java.term.project;
+--Table structure of table WritingGroups
+CREATE TABLE WritingGroups (
+    GroupName  varchar(100) NOT NULL,
+    HeadWriter varchar(30),
+    YearFormed varchar(4),
+    Subject    varchar(20),
+    CONSTRAINT WritingGroups_pk PRIMARY KEY (GroupName)
+ );
 
-import java.sql.*;
-import java.util.Scanner;
+--Table structure of table Publishers
+CREATE TABLE Publishers (
+    PublisherName varchar(100) NOT NULL,
+    PublisherAddress varchar (100),
+    PublisherPhone   varchar (20),
+    PublisherEmail varchar (100),
+    CONSTRAINT Publishers_pk PRIMARY KEY (PublisherName)
+);
 
-/**
- *
- * @author Mimi Opkins with some tweaking from Dave Brown
- */
-public class JDBC {
-    static Scanner in = new Scanner(System.in);
-    static final String displayFormat="%-35s%-50s%-25s%-25s\n";
-// JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
-    static String DB_URL = "jdbc:derby://localhost:1527/CECS_323_JDBC";
-//            + "testdb;user=";
-/**
- * Takes the input string and outputs "N/A" if the string is empty or null.
- * @param input The string to be mapped.
- * @return  Either the input string or "N/A" as appropriate.
- */
-    public static String dispNull (String input) {
-        //because of short circuiting, if it's null, it never checks the length.
-        if (input == null || input.length() == 0)
-            return "N/A";
-        else
-            return input;
-    }
-    
-    public static void main(String[] args) throws SQLException {
-        //Prompt the user for the database name, and the credentials.
-        //If your database has no credentials, you can update this code to 
-        //remove that from the connection string.
-        Scanner in = new Scanner(System.in);
-        Connection conn = null; //initialize the connection
-        Statement stmt = null;  //initialize the statement that we're using
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
+--Table structure of table Books
+CREATE TABLE Books (
+    GroupName varchar(100) NOT NULL,
+    BookTitle varchar(100)NOT NULL,
+    PublisherName varchar (100) NOT NULL,
+    YearPublished varchar (4),
+    NumberPages   varchar (4),
+    CONSTRAINT Books_pk PRIMARY KEY (GroupName, BookTitle),
+    CONSTRAINT Books_fk FOREIGN KEY (GroupName) REFERENCES WritingGroups (GroupName),
+    CONSTRAINT Books_fk2 FOREIGN KEY (PublisherName) REFERENCES Publishers (PublisherName)
+);
 
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL);
+--Data for the table WritingGroups
 
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            Boolean quit = true;
-            String Menu = ("Functions for this database\n"
-                + "1) List all writing groups\n"
-                + "2) List all the data for a group specified by the user\n"
-                + "3) List all publishers\n"
-                + "4) List all the data for a publisher specified by the user\n"
-                + "5) List all book titles\n"
-                + "6) List all the data for a book specified by the user\n"
-                + "7) Insert a new book\n"
-                + "8) Insert a new publisher\n"
-                + "9) Delete a book\n"
-                + "Enter your choice");
-            while(quit)
-            {
-            System.out.println(Menu);
-            int input = in.nextInt();
-            if (input == 1) {
-                listWritingGroups(stmt);
-            }
-            else if (input == 2) {
-                listSpecifiedWritingGroups(stmt);
-            }
-            else if (input == 3) {
-                listPublishers(stmt);
-            }
-            else if (input == 4) {
-                listSpecifiedPublishers(stmt);
-            }
-            else if (input == 5) {
-                listBooks(stmt);
-            }
-            else if (input == 6) {
-                listSpecifiedBooks(stmt);
-            }
-            else if (input == 7) {
-                insertBook(stmt);
-            }
-            else if (input == 8) {
-                insertPublisher(stmt);
-            }
-            else if (input == 9) {
-                deleteSpecifiedBooks(stmt);
-            }
-            else {
-                System.out.println("Invalid input");
-            }}
-            
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-            } catch (SQLException se2) {
-            }// nothing we can do
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//end main
-    public static void listWritingGroups (Statement stmt) throws SQLException{
-       String sql;
-            sql = "SELECT GroupName FROM WritingGroups";
-            ResultSet rs = stmt.executeQuery(sql);
-        System.out.printf("%-25s\n", "Group Name");
-            while (rs.next()) {
-                //Retrieve by column name
-                String groupName = rs.getString("GroupName");
-                //String headWriter = rs.getString("HeadWriter");
-                //String yearFormed = rs.getString("YearFormed");
-                //String subject = rs.getString("Subject");
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Los Angeles Writing Group', 'John Smith', '2009', 'Fiction');
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Santa Monica Writing Group', 'Michael Williams', '2012', 'Nonfiction');
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Garden Grove Writing Group', 'David Robinson', '2011', 'Romance');
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Long Beach Writing Group', 'Mary Jones', '2014', 'Horror');
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Santa Ana Writing Group', 'Sophia Taylor', '2006', 'Mystery');
+INSERT INTO WritingGroups(GroupName, HeadWriter, YearFormed, Subject) VALUES 
+('Fullerton Writing Group', 'Isabella Hernandez', '2004', 'Drama');
 
-                //Display values
-                System.out.printf("%-25s\n", 
-                        dispNull(groupName));
-        }
-    }
-    public static void listPublishers (Statement stmt) throws SQLException{
-        String sql; 
-        sql = "SELECT PublisherName FROM Publishers"; 
-        ResultSet rs = stmt.executeQuery(sql);
-        System.out.printf("%-25s\n", "Publisher Name"); 
-        while (rs.next()) { 
-//Retrieve by column name 
-            String publisherName = rs.getString("PublisherName"); 
-            //String publisherAddress = rs.getString("PublisherAddress"); 
-            //String publisherPhone = rs.getString("PublisherPhone"); 
-            //String publisherEmail = rs.getString("PublisherEmail"); 
-        System.out.printf("%-25s\n", 
-                dispNull(publisherName));
-        }
-    }
-    public static void listBooks (Statement stmt) throws SQLException{
-        String sql; 
-        sql =  "SELECT BookTitle FROM Books";; 
-        ResultSet rs = stmt.executeQuery(sql);
-        System.out.printf("%-25s\n",  "Book Title"); 
-        while (rs.next()) { 
-//Retrieve by column name 
-            //String groupName = rs.getString("GroupName"); 
-            String bookTitle = rs.getString("BookTitle"); 
-            //String publisherName = rs.getString("PublisherName"); 
-            //String yearPublished = rs.getString("YearPublished");
-            //String numberOfPages = rs.getString("NumberPages");
-        System.out.printf("%-25s\n", 
-                 dispNull(bookTitle));
-        }
-    }
-    public static void listSpecifiedWritingGroups (Statement stmt) throws SQLException{
-      
-       System.out.println("Which group would you like to look up: "); 
-       String Group = in.nextLine();
-       String sql;
-       sql = "SELECT * FROM WritingGroups WHERE GroupName ='"+Group+"'";
-       ResultSet rs = stmt.executeQuery(sql);
-       System.out.printf(displayFormat, "Group Name", "Head Writer", "Year Formed", "Subject");
-       while (rs.next()) {
-                //Retrieve by column name
-                String groupName = rs.getString("GroupName");
-                String headWriter = rs.getString("HeadWriter");
-                String yearFormed = rs.getString("YearFormed");
-                String subject = rs.getString("Subject");
+--Data for the table Publishers
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Lion Publishing', '123 6th St. Melbourne, FL 32904','239-200-6398','contact@LionPublishing.com');
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Koala Pages','71 Pilgrim Avenue Chevy Chase, MD 20815','240-200-3745','contact@KoalaPages.org');
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Bear Corporation','70 Bowman St. South Windsor, CT 06074','203-200-7944','contact@BearCorp.com');
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Turtle Company','4 Goldfield Rd. Honolulu, HI 96815','808-200-1015','contact@TurtleCo.net');
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Owl Press','44 Shirley Ave. West Chicago, IL 60185','217-200-8931','contact@OwlPress.org');
+INSERT INTO Publishers (PublisherName, PublisherAddress, PublisherPhone, PublisherEmail) VALUES
+('Bumblebee Books','514 S. Magnolia St. Orlando, FL 32806','239-200-5251','contact@BBBooks.com');
 
-                //Display values
-                System.out.printf(displayFormat, 
-                        dispNull(groupName), dispNull(headWriter), dispNull(yearFormed), dispNull(subject));
-        }
-    }
-    public static void listSpecifiedPublishers (Statement stmt) throws SQLException{
-        System.out.println("Which publisher would you like to look up: "); 
-        String PublisherName = in.nextLine();
-        String sql; 
-        sql = "SELECT * FROM Publishers WHERE PublisherName ='"+PublisherName+"'"; 
-        ResultSet rs = stmt.executeQuery(sql);
-        System.out.printf(displayFormat, "Publisher Name", "Publisher Address", "Publisher Phone", "Publisher Email"); 
-        while (rs.next()) { 
-//Retrieve by column name 
-            String publisherName = rs.getString("PublisherName"); 
-            String publisherAddress = rs.getString("PublisherAddress"); 
-            String publisherPhone = rs.getString("PublisherPhone"); 
-            String publisherEmail = rs.getString("PublisherEmail"); 
-        System.out.printf(displayFormat, 
-                dispNull(publisherName), dispNull(publisherAddress), dispNull(publisherPhone), dispNull(publisherEmail));
-        }
-    }
-    public static void listSpecifiedBooks (Statement stmt) throws SQLException{
-      
-       System.out.println("Which book would you like to look up: "); 
-       String BookName = in.nextLine();
-       String sql;
-       sql = "SELECT * FROM Books WHERE BookTitle ='"+BookName+"'";
-       ResultSet rs = stmt.executeQuery(sql);
-       System.out.printf(displayFormat, "Group Name", "Book Title", "Publisher Name" ,"Year Published", "Number of Pages"); 
-       while (rs.next()) { 
-//Retrieve by column name 
-            String groupName = rs.getString("GroupName"); 
-            String bookTitle = rs.getString("BookTitle"); 
-            String publisherName = rs.getString("PublisherName"); 
-            String yearPublished = rs.getString("YearPublished");
-            String numberOfPages = rs.getString("NumberPages");
-        System.out.printf(displayFormat, 
-                dispNull(groupName), dispNull(bookTitle), dispNull(publisherName), dispNull(yearPublished), dispNull(numberOfPages));
-        }
-    }
-    public static void deleteSpecifiedBooks (Statement stmt) throws SQLException{
-        
-       Connection conn = DriverManager.getConnection(DB_URL); 
-       System.out.println("Which book would you like to delete: "); 
-       String BookName = in.nextLine();
-       String sql;
-       sql = "DELETE FROM Books WHERE BookTitle = ?";
-       PreparedStatement pstmt = conn.prepareStatement(sql);
-       pstmt.setString(1, BookName);
-       pstmt.executeUpdate();
-    }
-    public static void insertBook(Statement stmt) throws SQLException
-    {
-        Connection conn = DriverManager.getConnection(DB_URL);
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the group name:");
-        String group = in.nextLine();
-        System.out.println("Enter the book title:");
-        String title = in.nextLine();
-        System.out.println("Enter the publisher name:");
-        String publisher = in.nextLine();
-        System.out.println("Enter the year published:");
-        String year = in.nextLine();
-        System.out.println("Enter the number of pages:");
-        String page = in.nextLine();
-
-       String sql;
-       
-       sql = "INSERT INTO Books ( GroupName, BookTitle, PublisherName ,YearPublished, NumberPages) VALUES" + 
-               "(?,?,?,?,?)";
-       PreparedStatement pstmt = conn.prepareStatement(sql);
-       pstmt.setString(1, group);
-       pstmt.setString(2, title);
-       pstmt.setString(3, publisher);
-       pstmt.setString(4, year);
-       pstmt.setString(5, page);
-       pstmt.executeUpdate();
-    }
-    public static void insertPublisher(Statement stmt) throws SQLException
-    {
-        Connection conn = DriverManager.getConnection(DB_URL);
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the publisher name: ");
-        String pName = in.nextLine();
-        System.out.println("Enter the publisher address: ");
-        String pAddress = in.nextLine();
-        System.out.println("Enter the publisher phone: ");
-        String pPhone = in.nextLine();
-        System.out.println("Enter the publisher email: ");
-        String pEmail = in.nextLine();
-        System.out.println("Which publisher would you like to replace: ");
-        String pReplace = in.nextLine();
-     
-
-       String sql;
-       
-       sql = "INSERT INTO Publishers ( PublisherName ,PublisherAddress, PublisherPhone, PublisherEmail) VALUES" + 
-               "(?,?,?,?)";
-       PreparedStatement pstmt = conn.prepareStatement(sql);
-       pstmt.setString(1, pName);
-       pstmt.setString(2, pAddress);
-       pstmt.setString(3, pPhone);
-       pstmt.setString(4, pEmail);
-       pstmt.executeUpdate();
-       
-       String sql2;
-       sql2 = "UPDATE Books SET PublisherName = ? WHERE PublisherName = ?";
-       PreparedStatement pstmt2 = conn.prepareStatement(sql2);
-       pstmt2.setString(1, pName);
-       pstmt2.setString(2, pReplace);
-       pstmt2.executeUpdate();
-    }
-}//end FirstExample}
+--Data for the table Books
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Los Angeles Writing Group','Throne Games','Lion Publishing','2011','523');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Santa Monica Writing Group','History of Airplanes','Bear Corporation','2017','356');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Los Angeles Writing Group','Dr. Robot','Lion Publishing','2013','201');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Garden Grove Writing Group','Love','Turtle Company','2015','236');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Long Beach Writing Group','Monster 2','Koala Pages','2018','278');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Santa Ana Writing Group','Cabin 4','Owl Press','2007','270');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Los Angeles Writing Group','East World','Koala Pages','2011','369');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Fullerton Writing Group','The Room','Owl Press','2005','197');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Santa Ana Writing Group','Night','Bumblebee Books','2009','237');
+INSERT INTO Books (GroupName, BookTitle, PublisherName, YearPublished, NumberPages) VALUES
+('Long Beach Writing Group','Monster','Koala Pages','2014','298');
